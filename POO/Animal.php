3bbea54b23f2af19c0a1c835842b1294617b46c0
lucas <ltozzioliveira>
2024-08-19 22:10:57 -0300

    19/08

---
+++ POO/Animal.php
@@ -0,0 +1,35 @@
+<?php
+
+    abstract class Animal {
+        protected $nome;
+        protected $raca;
+
+        abstract public function fazerBarulho();
+
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function setRaca($raca){
+            $this->raca = $raca;
+        }
+
+        public function getRaca(){
+            return $this->raca;
+        }
+    }
+
+    class Cachorro extends Animal {
+        private $pelagem;
+        //get e set...
+
+        public function fazerBarulho(){
+            return "Au Au!";
+        }
+
+        
+    }
